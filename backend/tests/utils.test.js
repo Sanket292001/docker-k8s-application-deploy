91e53fca98a468309b064d34d0f8200a216e0fef
Sanket Salunke <salunkess2001@gmail.com>
2023-06-18 18:03:31 +0530

    Resolved test issue
    Signed-off-by: Sanket Salunke <salunkess2001@gmail.com>

--- backend/tests/utils.test.js
+++ backend/tests/utils.test.js
@@ -4,7 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(utils.sum(1, 2)).toBe(3);
 });
 
-// Test - will FAIL
-test('subs 1 - 2 to equal 1', () => {
-  expect(utils.sub(1, 2)).toBe(1);
+test('subs 1 - 2 to equal -1', () => {
+  expect(utils.sub(1, 2)).toBe(-1);
 });
