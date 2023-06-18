81c548e284571ffae7049ad664cd4bf5534ac64b
Sanket Salunke <salunkess2001@gmail.com>
2023-06-18 17:59:26 +0530

    Added false statements
    Signed-off-by: Sanket Salunke <salunkess2001@gmail.com>

---
+++ backend/tests/utils.test.js
@@ -0,0 +1,10 @@
+const utils = require("./utils");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(utils.sum(1, 2)).toBe(3);
+});
+
+// Test - will FAIL
+test("subs 1 - 2 to equal 1", () => {
+  expect(utils.sub(1, 2)).toBe(1);
+});
